818b8b152e97f6144d07c6e3d3e88e0e443a57b8
dannash100 <danster15@gmail.com>
2018-09-03 20:13:36 +1200

    move left and right

--- ship.py
+++ ship.py
@@ -11,12 +11,16 @@ class Ship():
 
         # movement flags
         self.moving_right = False
+        self.moving_left = False
+
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
     def update(self):
         if self.moving_right:
             self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
