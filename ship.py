cbbb087d181e496100db026a57d11a850a35310e
dannash100 <danster15@gmail.com>
2018-09-03 20:10:28 +1200

    movment flags for continous movement

--- ship.py
+++ ship.py
@@ -9,8 +9,14 @@ class Ship():
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
+        # movement flags
+        self.moving_right = False
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+    def update(self):
+        if self.moving_right:
+            self.rect.centerx += 1
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
