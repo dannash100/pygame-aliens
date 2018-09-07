1b2edb9dd13e4ac52f8c648101cde9fd16b3211b
dannash100 <danster15@gmail.com>
2018-09-07 12:52:11 +1200

    Bullet class and settings

--- settings.py
+++ settings.py
@@ -7,5 +7,11 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (0, 0, 0)
 
-        # Ship Settings
+        # Ship settings
         self.ship_speed_factor = 1.5
+
+        # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_hight = 15
+        self.bullet_color = 60, 60, 60
