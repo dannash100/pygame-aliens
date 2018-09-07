efc099bd0bd633f11bd883a925acc6d402b46602
dannash100 <danster15@gmail.com>
2018-09-07 12:26:32 +1200

    continuous movement

--- ship.py
+++ ship.py
@@ -12,24 +12,22 @@ class Ship():
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
-        self.center = float(self.rect.centerx)
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
 
+        self.center = float(self.rect.centerx)
         # movement flags
         self.moving_right = False
         self.moving_left = False
 
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
-
     def update(self):
 
-        if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.rect.centerx += self.ai_settings.ship_speed_factor
-        if self.moving_left and self.rect.left > 0:
-            self.rect.centerx -= self.ai_settings.ship_speed_factor
+        if self.moving_right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left:
+            self.center -= self.ai_settings.ship_speed_factor
 
         self.rect.centerx = self.center
 
     def blitme(self):
-
         self.screen.blit(self.image, self.rect)
